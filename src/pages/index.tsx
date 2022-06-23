55e77bf105b8491ba347fba2f256db9839c7071b
Dmitry Klyushkov <charlesfxa@gmail.com>
2022-06-24 00:55:39 +0300

    getAllQuestions route is added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,15 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 
-import { prisma } from "../db/client";
 import { trpc } from "../utils/trpc";
 
 const Home: NextPage = (props: any) => {
-  const { data, isLoading } = trpc.useQuery(["hi"]);
+  const { data, isLoading } = trpc.useQuery(["getAllQuestions"]);
 
   if (isLoading || !data) return <div>Loading...</div>;
 
-  return <div>{data.greeting}</div>;
-};
+  console.log(data);
 
-export const getServerSideProps = async () => {
-  const questions = await prisma.pollQuestion.findMany();
-
-  return {
-    props: {
-      questions: JSON.stringify(questions),
-    },
-  };
+  return <div>{data[0]?.question}</div>;
 };
 
 export default Home;
