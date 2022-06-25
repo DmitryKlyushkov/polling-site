92cc7d1db0e15d4b974fba043968603aacc51820
Dmitry Klyushkov <charlesfxa@gmail.com>
2022-06-25 15:47:05 +0300

    url and other things change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,27 @@
 import type { NextPage } from "next";
+import { useRef } from "react";
 
 import { trpc } from "../utils/trpc";
 
 const QuestionCreator: React.FC = () => {
-  const { mutate } = trpc.useMutation("questions.create");
+  const client = trpc.useContext();
+  const inputRef = useRef<HTMLInputElement>(null);
+  const { mutate, isLoading } = trpc.useMutation("questions.create", {
+    onSuccess: () => {
+      client.invalidateQueries(["questions.get-all"]);
+      if (!inputRef.current) return;
+      inputRef.current.value = "";
+    },
+  });
 
   return (
     <input
-      onSubmit={(e) => {
-        console.log(e.currentTarget.value);
+      ref={inputRef}
+      disabled={isLoading}
+      onKeyDown={(e) => {
+        if (e.key === "Enter") {
+          mutate({ question: e.currentTarget.value });
+        }
       }}
     />
   );
@@ -22,7 +35,15 @@ const Home: NextPage = () => {
   return (
     <div className="flex flex-col">
       <div className="text-2xl font-bold">Questions</div>
-      <div>{data[0]?.question}</div>
+      <div>
+        {data.map((question) => {
+          return (
+            <div key={question.id} className="my-2">
+              {question.question}
+            </div>
+          );
+        })}
+      </div>
       <QuestionCreator />
     </div>
   );
