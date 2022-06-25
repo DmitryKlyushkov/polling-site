d9331f4bc45d1b3b83ed7bc69305167d015649a2
Dmitry Klyushkov <charlesfxa@gmail.com>
2022-06-25 14:54:54 +0300

    Reformat backend

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,9 @@ import type { NextPage } from "next";
 import { trpc } from "../utils/trpc";
 
 const Home: NextPage = (props: any) => {
-  const { data, isLoading } = trpc.useQuery(["getAllQuestions"]);
+  const { data, isLoading } = trpc.useQuery(["questions.get-all"]);
 
   if (isLoading || !data) return <div>Loading...</div>;
-
-  console.log(data);
 
   return <div>{data[0]?.question}</div>;
 };
