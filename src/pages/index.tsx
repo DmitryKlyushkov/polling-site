28fbe78dc3b20ea8230e9c09e1c0d1cebef43a7b
Dmitry Klyushkov <charlesfxa@gmail.com>
2022-06-25 15:25:48 +0300

    tailwindCSS import fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,30 @@ import type { NextPage } from "next";
 
 import { trpc } from "../utils/trpc";
 
+const QuestionCreator: React.FC = () => {
+  const { mutate } = trpc.useMutation("questions.create");
+
+  return (
+    <input
+      onSubmit={(e) => {
+        console.log(e.currentTarget.value);
+      }}
+    />
+  );
+};
+
 const Home: NextPage = () => {
   const { data, isLoading } = trpc.useQuery(["questions.get-all"]);
 
   if (isLoading || !data) return <div>Loading...</div>;
 
-  return <div>{data[0]?.question}</div>;
+  return (
+    <div className="flex flex-col">
+      <div className="text-2xl font-bold">Questions</div>
+      <div>{data[0]?.question}</div>
+      <QuestionCreator />
+    </div>
+  );
 };
 
 export default Home;
