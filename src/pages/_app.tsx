61ef16c1d8f5a33c46b75fe83724820237796ed3
Dmitry Klyushkov <charlesfxa@gmail.com>
2022-06-27 17:02:36 +0300

    tRPC SSR

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,14 +9,24 @@ const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
 };
 
+function getBaseUrl() {
+  if (typeof window !== "undefined") {
+    return "";
+  }
+  if (process.browser) return ""; // Browser should use correct path
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use Vercel url
+
+  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+}
+
 export default withTRPC<AppRouter>({
-  config({ ctx }) {
-    const url = "/api/trpc";
+  config() {
+    const url = `${getBaseUrl()}/api/trpc`;
 
     return {
       url,
       transformer: superjson,
     };
   },
-  ssr: false,
+  ssr: true,
 })(MyApp);
