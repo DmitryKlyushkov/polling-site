bc05f223d39068b1265bc87ec15c208fc2790c43
Dmitry Klyushkov <charlesfxa@gmail.com>
2022-06-28 14:01:24 +0300

    remove console log)

--- src/backend/router/context.ts
+++ src/backend/router/context.ts
@@ -3,8 +3,6 @@ import * as trpcNext from "@trpc/server/adapters/next";
 
 // The app's context - is generated for each incoming request
 export async function createContext(opts?: trpcNext.CreateNextContextOptions) {
-  console.log("Token", opts?.req.cookies["poll-token"]);
-
   return { token: opts?.req.cookies["poll-token"] };
 }
 type Context = trpc.inferAsyncReturnType<typeof createContext>;
