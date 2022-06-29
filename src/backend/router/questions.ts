9178423e7b3ade7eb04d2c492db0a280420fc56c
Dmitry Klyushkov <charlesfxa@gmail.com>
2022-06-29 15:59:35 +0300

    fixes

--- src/backend/router/questions.ts
+++ src/backend/router/questions.ts
@@ -41,6 +41,7 @@ export const questionRouter = createRouter()
 
       if (rest.vote || rest.isOwner) {
         const votes = await prisma.vote.groupBy({
+          where: { questionId: input.id },
           by: ["Choice"],
           _count: true,
         });
